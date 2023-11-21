9497eec76b7f1484e4a12ab2e1c22eacebc83c00
Kok, Chern Yee <chernyee.kok@rapidsilicon.com>
2023-11-21 16:51:01 +0800

    integrate hardware manager class

---
+++ src/Configuration/HardwareManager/OpenocdAdapter.h
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 The Foedag team
+
+GPL License
+
+Copyright (c) 2023 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __OPENOCDADAPTER_H__
+#define __OPENOCDADAPTER_H__
+
+#include <atomic>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "JtagAdapter.h"
+
+using CommandExecutorFuncType = std::function<int(
+    const std::string &, std::string &, std::ostream *, std::atomic<bool> &)>;
+
+class OpenocdAdapter : public JtagAdapter {
+ public:
+  OpenocdAdapter(std::string openocd_filepath,
+                 CommandExecutorFuncType command_executor)
+      : m_openocd_filepath(openocd_filepath),
+        m_command_executor(command_executor) {}
+  virtual std::vector<uint32_t> scan(const Cable &cable);
+
+ private:
+  int execute(const Cable &cable, std::string &output);
+  std::string convert_transport_to_string(TransportType transport,
+                                          std::string defval = "jtag");
+  std::string m_openocd_filepath;
+  CommandExecutorFuncType m_command_executor;
+};
+
+#endif  //__OPENOCDADAPTER_H__
